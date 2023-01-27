6367a97a48230586cb007b5c4f15ab06a1b7b50f
7ijme <tijmevh@gmail.com>
2023-01-27 17:29:37 +0100

    mouse hover

--- src/Components/Grid.tsx
+++ src/Components/Grid.tsx
@@ -27,6 +27,22 @@ export type BoxType =
   | "checking";
 
 export default function Grid({ grid, setGrid, columns, rows }: Props) {
+  const handleBoxClick = (rowIndex: number, boxIndex: number) => {
+    const newGrid = [...grid];
+    switch (newGrid[rowIndex][boxIndex].type) {
+      case "empty":
+        newGrid[rowIndex][boxIndex].type = "wall";
+        break;
+      case "wall":
+        newGrid[rowIndex][boxIndex].type = "empty";
+        break;
+      default:
+        break;
+    }
+    console.log(newGrid);
+    setGrid(newGrid);
+  };
+
   return (
     <>
       <div className="grid-wrapper">
@@ -42,21 +58,14 @@ export default function Grid({ grid, setGrid, columns, rows }: Props) {
                 <div
                   key={box.id}
                   className={`box ${box.type}`}
-                  onClick={() => {
-                    const newGrid = [...grid];
-                    switch (newGrid[rowIndex][boxIndex].type) {
-                      case "empty":
-                        newGrid[rowIndex][boxIndex].type = "wall";
-                        break;
-                      case "wall":
-                        newGrid[rowIndex][boxIndex].type = "empty";
-                        break;
-                      default:
-                        break;
-                    }
-                    console.log(newGrid);
-                    setGrid(newGrid);
-                  }}></div>
+                  onMouseDown={(e) => {
+                    if (e.buttons == 1) handleBoxClick(rowIndex, boxIndex);
+                  }}
+                  onTouchMove={handleBoxClick.bind(null, rowIndex, boxIndex)}
+                  onMouseOver={(e) => {
+                    if (e.buttons == 1) handleBoxClick(rowIndex, boxIndex);
+                  }}
+                />
               );
             });
           })}
